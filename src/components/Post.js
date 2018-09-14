e4a57afd177d20b17afabd6dc1c209bc82078c20
Ramón Souza <contato@ramonsouza.com>
2018-09-13 23:33:09 -0300

    base funcionalidade 'like'

--- src/components/Post.js
+++ src/components/Post.js
@@ -15,7 +15,8 @@ import {
   ScrollView,
   Image, 
   Dimensions,
-  FlatList
+  FlatList,
+  TouchableOpacity,
 } from 'react-native';
 
 const width = Dimensions.get('screen').width;
@@ -31,18 +32,39 @@ export default class Post extends Component<Props> {
     }
   }
 
+  carregaIcone(likeada) {
+    return likeada ? require('../../resources/img/s2-checked.png') : 
+    require('../../resources/img/s2.png')
+  }
+
+  like() {
+    const fotoAtualizada = {
+        //TODO: pesquisar sobre isso depois (spread syntax)
+        ...this.state.foto,
+        likeada: !this.state.foto.likeada
+    }
+    this.setState({foto: fotoAtualizada})
+  }
+
   render() {
     const { foto } = this.state;
     
     return (
         <View>
-        <View style={styles.cabecalho} >
-            <Image source={{uri: foto.urlPerfil}} 
-            style={styles.fotoDePerfil}/>
-            <Text>{foto.loginUsuario}</Text>
-        </View>
-        <Image source={{uri: foto.urlFoto}} 
-            style={styles.foto}/>
+            <View style={styles.cabecalho} >
+                <Image source={{uri: foto.urlPerfil}} 
+                style={styles.fotoDePerfil}/>
+                <Text>{foto.loginUsuario}</Text>
+            </View>
+            <Image source={{uri: foto.urlFoto}} 
+                style={styles.foto}/>
+            <View style={styles.rodape} >
+                <TouchableOpacity onPress={this.like.bind(this)}>
+                    <Image style={styles.botaoDeLike} 
+                        source={this.carregaIcone(foto.likeada)} 
+                    />
+                </TouchableOpacity>
+            </View>
         </View>
     );
   }
@@ -64,4 +86,11 @@ const styles = StyleSheet.create({
     width: width, 
     height: width
   },
+  botaoDeLike: {
+    width: 40,
+    height: 40
+  },
+  rodape: {
+    margin: 10
+  }
 });
