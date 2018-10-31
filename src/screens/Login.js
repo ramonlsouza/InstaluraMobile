ae30f813e3bcf78ba9c0ba6027312a6d492e45b2
Ramón Souza <contato@ramonsouza.com>
2018-10-30 22:47:24 -0300

    funcionalidade basica login, retornando token da api

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -12,17 +12,46 @@ import {
 const width = Dimensions.get('screen').width;
 
 export default class Login extends Component{
+    efetuaLogin(){
+        const uri = "https://instalura-api.herokuapp.com/api/public/login";
+        const requestInfo = {
+            method: 'POST',
+            body: JSON.stringify({
+                login: this.state.usuario,
+                senha: this.state.senha,
+            }),
+            headers: new Headers({
+                'Content-type': 'application/json'
+            })
+        }
+        fetch(uri, requestInfo)
+            .then(response => {
+                if(response.ok)
+                    return response.text()
+
+                throw new Error("Não foi possível efetuar login.");
+            })
+            .then(token => console.warn(token))
+    }
     render(){
         return(
             <View style={styles.container}>
                 <Text style={styles.titulo}>Instalura</Text>
                 <View style={styles.form}>
-                    <TextInput style={styles.input} placeholder="Usuário"
-                        onChangeText={texto => this.setState({usuario : texto})} />
-                    <TextInput style={styles.input} placeholder="Senha"
-                        onChangeText={texto => this.setState({senha : texto})} />
+                    <TextInput 
+                        style={styles.input} 
+                        placeholder="Usuário"
+                        onChangeText={texto => this.setState({usuario : texto})} 
+                        autoCapitalize="none"
+                    />
+                    <TextInput 
+                        style={styles.input} 
+                        placeholder="Senha"
+                        onChangeText={texto => this.setState({senha : texto})} 
+                        secureTextEntry={true}
+                    />
                         
-                    <Button title="Login" onPress={() => console.warn("Login")} />
+                    <Button title="Login" onPress={this.efetuaLogin.bind(this)} />
                 </View>
             </View>
         );
