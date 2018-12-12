3223f556cdc9dc401dba8c78caf1d47f492ee5ad
Ramón Souza <contato@ramonsouza.com>
2018-12-11 23:40:26 -0200

    fix like em foto com usuario logado

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -44,30 +44,48 @@ export default class Feed extends Component{
   like(idFoto) {
     const foto = this.state.fotos.find(foto => foto.id === idFoto)
 
-    let novaLista = []
+    AsyncStorage.getItem('usuario')
+    .then(usuarioLogado => {
+      let novaLista = []
 
-    if(!foto.likeada){
-      novaLista = [
-        ...foto.likers,
-        {login: 'trivialidades'}
-      ]
-    }else{
-      novaLista = foto.likers.filter( liker => {
-        return liker.login !== 'trivialidades'
-      });
-    }
+      if(!foto.likeada){
+        novaLista = [
+          ...foto.likers,
+          {login: usuarioLogado}
+        ]
+      }else{
+        novaLista = foto.likers.filter( liker => {
+          return liker.login !== usuarioLogado
+        });
+      }
 
-
-    const fotoAtualizada = {
+      return novaLista;
+    })
+    .then(novaLista => {
+      const fotoAtualizada = {
         //TODO: pesquisar sobre isso depois (spread syntax)
         ...foto,
         likeada: !foto.likeada,
         likers: novaLista
-    }
-    const fotos = this.state.fotos
-      .map( foto => foto.id === fotoAtualizada.id ? fotoAtualizada : foto)
+      }
+      const fotos = this.state.fotos
+        .map( foto => foto.id === fotoAtualizada.id ? fotoAtualizada : foto)
 
-    this.setState({fotos})
+      this.setState({fotos})
+    })
+
+    const uri = 'https://instalura-api.herokuapp.com/api/fotos/'+idFoto+'/like';
+
+    AsyncStorage.getItem('token')
+    .then(token => {
+      return {
+        method: 'POST',
+        headers: new Headers({
+          "X-AUTH-TOKEN": token
+        })
+      }
+    })
+    .then(requestInfo => fetch(uri,requestInfo))
   }
 
 
